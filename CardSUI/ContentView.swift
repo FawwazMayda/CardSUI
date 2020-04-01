b3be4189b9d2dee24a35bd89e885165138db41e7
fawwazmayda <maydafawwaz@gmail.com>
2020-04-01 23:13:36 +0800

    Bang Bola

--- CardSUI/ContentView.swift
+++ CardSUI/ContentView.swift
@@ -10,7 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Color(red: 0.09, green: 0.63, blue: 0.52).edgesIgnoringSafeArea(.top)
+            VStack {
+                Image("germanyball").resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 250, height: 250)
+                
+                
+                Text("Abang Bola")
+                    .font(.largeTitle).foregroundColor(.white)
+                Divider()
+                Text("CountryBall Developer")
+                    .font(.largeTitle).foregroundColor(.white)
+                
+                RoundedRectangle(cornerRadius: 15.0)
+                    .fill(Color.white)
+                    .frame(height: 50.0)
+                .overlay(
+                    HStack{
+                        Image(systemName: "phone.fill")
+                            .foregroundColor(.green)
+                            .frame(width: 45.0, height: 45.0)
+                        Text("Bola Oh Bola Mengapa Engkau Bola").font(.headline)
+                    }
+                    
+                )
+                
+            }
+        }
     }
 }
 
